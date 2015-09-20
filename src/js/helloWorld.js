72d4ccb1df33f2f0f067752d94c58d0f61eafdfa
Siim Valdaru <siim.valdaru@sorryasaservice.com>
2015-09-21 00:45:19 +0300

    load JS files after HTML files have been loaded.

---
+++ src/js/helloWorld.js
@@ -0,0 +1,27 @@
+function handleClick() {
+    console.log('request on its way');
+
+    $.ajax
+    ({
+        url: 'http://localhost:8080/api/hello',
+        type: 'get',
+        success: function (result) {
+            console.log('request successful');
+            console.log(result);
+            $('.response').text(result.key).fadeIn(700, function () {
+                setTimeout(function () {
+                    $('.response').fadeOut();
+                }, 100000);
+            });
+        },
+        error: function (result) {
+            console.log('request failed');
+            console.log(result);
+        }
+    });
+}
+
+$(document).ready(function () {
+    console.log('document loaded');
+    $('.request').click(handleClick);
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,39 +1,18 @@
 function loadAssets() { // jshint ignore:line
     $.holdReady(true);
-    var promises = [
+    var htmlPromises = [
         $('#pageContent').load('html/helloworld.html')
+    ];
+
+    $.when(htmlPromises).done(function () {
+        console.log('HTML loaded');
+
+        var jsPromises = [
+            $.getScript('js/helloWorld.js')
         ];
-
-    $.when(promises).done(function () {
-        console.log('Scripts loaded');
-        $.holdReady(false);
+        $.when(jsPromises).done(function () {
+            console.log('JS loaded');
+            $.holdReady(false);
+        });
     });
 }
-
-function handleClick() {
-    console.log('request on its way');
-
-    $.ajax
-    ({
-        url: 'http://localhost:8080/api/hello',
-        type: 'get',
-        success: function (result) {
-            console.log('request successful');
-            console.log(result);
-            $('.response').text(result.key).fadeIn(700, function () {
-                setTimeout(function () {
-                    $('.response').fadeOut();
-                }, 100000);
-            });
-        },
-        error: function (result) {
-            console.log('request failed');
-            console.log(result);
-        }
-    });
-}
-
-$(document).ready(function () {
-    console.log('document loaded');
-    $('.request').click(handleClick);
-});
