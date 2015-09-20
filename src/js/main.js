08eac51fe2856b14072fc39f41f8d992186c52ba
Siim Valdaru <siim.valdaru@sorryasaservice.com>
2015-09-20 23:33:44 +0300

    inital ajax GET request.

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,34 @@
+function handleClick() {
+    console.log('request on its way');
+
+    $.ajax
+    ({
+        url: 'reservebook.php',
+        type: 'get',
+        success: function (result) {
+            alert('request successful' + result);
+            /*$('.modal-box').text(result).fadeIn(700, function () {
+                setTimeout(function () {
+                    $('.modal-box').fadeOut();
+                }, 2000);
+            });*/
+        },
+        error: function (result) {
+            alert('request failed' + result);
+        }
+    });
+}
+
+function loaded() {
+    $(document).ready(function () {
+        console.log('loaded');
+
+        $('.request').click(handleClick);
+    });
+}
+
 $(function () {
-    $('#pageContent').load('html/helloworld.html');
+    $('#pageContent').load('html/helloworld.html', loaded);
 });
 
-$(document).ready(function () {
-    console.log('loaded');
-    $('.request').click(function () {
-        console.log('request on its way');
-    });
-});
+
