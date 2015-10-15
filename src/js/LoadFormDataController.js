394791f00b8955aaa323388bd22a8f4eb47e199b
Siim Valdaru <siim.valdaru@sorryasaservice.com>
2015-10-16 02:09:11 +0300

    Payment destination is chosen from initial GET response data

--- src/js/LoadFormDataController.js
+++ src/js/LoadFormDataController.js
@@ -3,7 +3,11 @@ $(document).ready(function () {
 
     $.getJSON('http://localhost:8080/api/faculty')
         .done(function (result) {
-            alert("me success");
+
+            result.object.forEach(function (faculty) {
+                $("#formFaculties").append('<div class="radio-inline"><label><input type="radio" name="faculty"><span class="facultyLabelName"></span></label></div>');
+                $(".facultyLabelName:last").text(faculty.name);
+            });
         })
         .fail(function (result) {
             alert("me failed");
