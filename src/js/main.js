d24beecb9527fe289f98590ba8a666f7e9b26ba7
silver17 <palumaa.silver@gmail.com>
2015-10-18 17:48:13 +0300

    adminPage

--- src/js/main.js
+++ src/js/main.js
@@ -12,3 +12,15 @@ function loadAssets() { // jshint ignore:line
         $.holdReady(false);
     });
 }
+
+function loadAssetsAdmin() {
+    $.holdReady(true);
+    var promisesAdmin = [
+        $('#pageContent').load('html/adminPage.html')
+    ];
+
+    $.when(promisesAdmin).done(function () {
+        console.log('admin loaded');
+        $holdReady(false);
+    })
+}
