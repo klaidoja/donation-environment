4909ceef64b172b356c447d982237229ff2b1d76
Siim Valdaru <siim.valdaru@sorryasaservice.com>
2015-10-20 00:39:27 +0300

    Admin's can now see all made donations and their details

--- src/js/AdminDonationViewController.js
+++ src/js/AdminDonationViewController.js
@@ -1,3 +1,25 @@
 function loadAdminDonationsView(){
     $('#pageContent').load('html/adminMainView.html');
+
+    $.getJSON('http://localhost:8080/api/donation')
+        .done(function (result) {
+            alert('GET request for all donations succeeded');
+            var $adminDonationTable = $('#adminDonationTable');
+            $adminDonationTable.empty();
+            result.object.forEach(function(donation){
+                var $donation = $('<tr>').appendTo($adminDonationTable);
+                $('<td>').text(donation.dateCreated).appendTo($donation);
+                $('<td>').text(donation.name).appendTo($donation);
+                $('<td>').text(donation.faculty).appendTo($donation);
+                $('<td>').text(donation.fund.name).appendTo($donation);
+                $('<td>').text(donation.nameIsPublic).appendTo($donation);
+                $('<td>').text(donation.additionalInformation).appendTo($donation);
+
+            });
+            //$('#adminDonationTable').append('<tr><td>my data</td><td>more data</td></tr>');
+            console.log(result);
+        })
+        .fail(function (result) {
+            alert("GET request for all donations failed");
+        });
 }
