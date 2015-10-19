7e054cdc667b1f31f1d968300ff23b2425c87603
Siim Valdaru <siim.valdaru@sorryasaservice.com>
2015-10-20 01:12:02 +0300

    Donation success handling now proper. Must figure out how to get JSON data out of the xml response. Although the response already should actually be a JSON object. Must look deeper into to it to find the underlying problem.

--- src/js/DonationFormController.js
+++ src/js/DonationFormController.js
@@ -1,13 +1,18 @@
-var successFunction = function (data, textStatus, jqXHR) {
+var successFunction = function (data) {
     alert('Donation wish saved');
+    var donationInformation = data;
+    console.log(data);
+    var $donationFormMainContent = $('#jumbo');
+    $donationFormMainContent.empty();
+    $($donationFormMainContent).append('<div>').text('Here comes the relevant info about making the donation');
+};
+
+var notFoundFunction = function (data) {
+    alert('This API endpoint was not found!');
     console.log(data);
 };
 
-var notFoundFunction = function (data, textStatus, jqXHR) {
-    alert('This API endpoint was not found!');
-};
-
-var validationFailedFunction = function(data, textStatus, jqXHR){
+var validationFailedFunction = function(data){
     var errorData = data.responseText.errors;
     alert(errorData);
     console.log(data);
