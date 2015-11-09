08005be9f119c84786f83394417b04ae3d6b1e71
Siim Valdaru <siim.valdaru@sorryasaservice.com>
2015-11-09 16:43:04 +0200

    Removed multiple id's with the same name in admin settings view, because that goes against the most basic rules of HTML!

--- src/js/AdminSettingsViewController.js
+++ src/js/AdminSettingsViewController.js
@@ -5,16 +5,16 @@ function loadAdminSettingsView() {
         .done(function (result) {
             result.object.forEach(function (faculty) {
                 var $adminFacultiesList = $('#adminFacultiesList');
-                $adminFacultiesList.append('<p><label>Organisatsiooni nimi:</label><span id="toTheLeft" class="adminSettingsFacultyName"></span> <button type="button" class="asText">Muuda</button></p>');
+                $adminFacultiesList.append('<p><label>Organisatsiooni nimi:</label><span class="adminSettingsFacultyName"></span> <button type="button" class="asText">Muuda</button></p>');
                 $(".adminSettingsFacultyName:last").text(faculty.name);
-                $adminFacultiesList.append('<p><label>Panga konto (IBAN):</label><span id="toTheLeft" class="adminSettingsFacultyIBAN"></span> <button type="button" class="asText">Muuda</button></p>');
+                $adminFacultiesList.append('<p><label>Panga konto (IBAN):</label><span class="adminSettingsFacultyIBAN"></span> <button type="button" class="asText">Muuda</button></p>');
                 $(".adminSettingsFacultyIBAN:last").text(faculty.bankAccountNr);
-                $adminFacultiesList.append('<div class="adminFundsList"><p id="adminFund">Fondid:</p></div>');
+                $adminFacultiesList.append('<div class="adminFundsList"><p class="adminFund">Fondid:</p></div>');
                 faculty.fund.forEach(function (fund) {
                     var $adminFundsList = $('.adminFundsList:last');
-                    $adminFundsList.append('<p><label>Nimi:</label><span id="toTheLeft" class="adminSettingsFundName"></span> <button type="button" class="asText">Muuda</button></p>');
+                    $adminFundsList.append('<p><label>Nimi:</label><span class="adminSettingsFundName"></span> <button type="button" class="asText">Muuda</button></p>');
                     $(".adminSettingsFundName:last").text(fund.name);
-                    $adminFundsList.append('<p><label>Kirjeldus:</label><span id="toTheLeft" class="adminSettingsFundProperty"></span> <button type="button" class="asText">Muuda</button></p>');
+                    $adminFundsList.append('<p><label>Kirjeldus:</label><span class="adminSettingsFundProperty"></span> <button type="button" class="asText">Muuda</button></p>');
                     $(".adminSettingsFundProperty:last").text(fund.paymentExplanation);
                 });
                 $adminFacultiesList.append('<hr>');
