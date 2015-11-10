85c510914969eda0b9c05dccf6f87ab5f97f29ac
Siim Valdaru <siim.valdaru@sorryasaservice.com>
2015-11-10 19:58:19 +0200

    Added some intuitive reference to admin organisations view. Organisations have delete/edit buttons and "add a fund button". Overall view has "add an organisation button". Delete/edit needs to be put on the view also for funds.

--- src/js/AdminSettingsViewController.js
+++ src/js/AdminSettingsViewController.js
@@ -3,23 +3,39 @@ function loadAdminSettingsView() {
 
     $.getJSON('http://localhost:8080/api/faculty')
         .done(function (result) {
+            var $adminFacultiesList = $('#adminFacultiesList');
             result.object.forEach(function (faculty) {
-                var $adminFacultiesList = $('#adminFacultiesList');
-                $adminFacultiesList.append('<p><label>Organisatsiooni nimi:</label><span class="adminSettingsFacultyName"></span> <button type="button" class="asText">Muuda</button></p>');
+
+                var facultyId = faculty.id;
+                var $adminSingleFacultyDiv = $('<div class="col-xs-12"></div>').attr('id', facultyId);
+
+                $adminFacultiesList.append($adminSingleFacultyDiv);
+                $adminSingleFacultyDiv.append('<p><label>Organisatsiooni nimi:</label><span class="adminSettingsFacultyName"></span></p>');
                 $(".adminSettingsFacultyName:last").text(faculty.name);
-                $adminFacultiesList.append('<p><label>Panga konto (IBAN):</label><span class="adminSettingsFacultyIBAN"></span> <button type="button" class="asText">Muuda</button></p>');
+
+                $adminSingleFacultyDiv.append('<p><label>Panga konto (IBAN):</label><span class="adminSettingsFacultyIBAN"></span></p>');
                 $(".adminSettingsFacultyIBAN:last").text(faculty.bankAccountNr);
-                $adminFacultiesList.append('<div class="adminFundsList"><p class="adminFund">Fondid:</p></div>');
+
+                $adminFacultiesList.append('<hr>');
+
+                $adminSingleFacultyDiv.append('<button type="button" class="btn-default">Muuda</button>');
+                $adminSingleFacultyDiv.append('<button type="button" class="btn-danger">Kustuta</button>');
+
+                $adminSingleFacultyDiv.append('<div class="adminFundsList"><p class="adminFund">Fondid:</p></div>');
                 faculty.fund.forEach(function (fund) {
                     var $adminFundsList = $('.adminFundsList:last');
-                    $adminFundsList.append('<p><label>Nimi:</label><span class="adminSettingsFundName"></span> <button type="button" class="asText">Muuda</button></p>');
+                    $adminFundsList.append('<p><label>Nimi:</label><span class="adminSettingsFundName"></span></p>');
                     $(".adminSettingsFundName:last").text(fund.name);
-                    $adminFundsList.append('<p><label>Kirjeldus:</label><span class="adminSettingsFundProperty"></span> <button type="button" class="asText">Muuda</button></p>');
+                    $adminFundsList.append('<p><label>Kirjeldus:</label><span class="adminSettingsFundProperty"></span></p>');
                     $(".adminSettingsFundProperty:last").text(fund.paymentExplanation);
                 });
+
+                $adminSingleFacultyDiv.append('<button type="button" class="btn-primary">Lisa fond</button>').data(facultyId);
                 $adminFacultiesList.append('<hr>');
 
             });
+
+            $adminFacultiesList.append('<button type="button" class="btn-success">Lisa organisatsioon</button>');
 
         })
         .fail(function (result) {
