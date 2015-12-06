9633cd0b9ce2d56708b83630cb45ac8b73436f3c
silver17 <palumaa.silver@gmail.com>
2015-12-06 21:51:25 +0200

    adminSettings CSS

--- src/js/AdminSettingsViewController.js
+++ src/js/AdminSettingsViewController.js
@@ -7,7 +7,7 @@ function loadAdminSettingsView() {
             result.object.forEach(function (faculty) {
 
                 var facultyId = faculty.id;
-                var $adminSingleFacultyDiv = $('<div class="col-xs-12"></div>').attr('id', facultyId);
+                var $adminSingleFacultyDiv = $('<div class="jumbotron"></div>').attr('id', facultyId);
 
                 $adminFacultiesList.append($adminSingleFacultyDiv);
                 $adminSingleFacultyDiv.append('<p><label>Organisatsiooni nimi:</label><span class="adminSettingsFacultyName"></span></p>');
@@ -16,10 +16,10 @@ function loadAdminSettingsView() {
                 $adminSingleFacultyDiv.append('<p><label>Panga konto (IBAN):</label><span class="adminSettingsFacultyIBAN"></span></p>');
                 $(".adminSettingsFacultyIBAN:last").text(faculty.bankAccountNr);
 
-                $adminFacultiesList.append('<hr>');
 
-                $adminSingleFacultyDiv.append('<button type="button" class="btn-default">Muuda</button>');
-                $adminSingleFacultyDiv.append('<button type="button" class="btn-danger">Kustuta</button>');
+
+                $adminSingleFacultyDiv.append('<button type="button" class="btn btn-info btn-xs nupp">Muuda</button>');
+                $adminSingleFacultyDiv.append('<button type="button" class="btn btn-warning btn-xs nupp">Kustuta</button>');
 
                 $adminSingleFacultyDiv.append('<div class="adminFundsList"><p class="adminFund">Fondid:</p></div>');
                 faculty.fund.forEach(function (fund) {
@@ -30,12 +30,12 @@ function loadAdminSettingsView() {
                     $(".adminSettingsFundProperty:last").text(fund.paymentExplanation);
                 });
 
-                $adminSingleFacultyDiv.append('<button type="button" class="btn-primary">Lisa fond</button>').data(facultyId);
+                $adminSingleFacultyDiv.append('<button type="button" class="btn btn-primary btn-xs nupp">Lisa fond</button>').data(facultyId);
                 $adminFacultiesList.append('<hr>');
 
             });
 
-            $adminFacultiesList.append('<button type="button" class="btn-success">Lisa organisatsioon</button>');
+            $adminFacultiesList.append('<button type="button" class="btn btn-primary btn-xs nupp">Lisa organisatsioon</button>');
 
         })
         .fail(function (result) {
